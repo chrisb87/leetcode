9f06cc8687ebfbae3668f59f1e0c4f7487105a13
Chris Baker <baker.chris.3@gmail.com>
2026-01-06 11:12:11 -0800

    another fix, needs cleanup

--- streamofcharacters.py
+++ streamofcharacters.py
@@ -8,21 +8,15 @@ class StreamChecker:
         for word in words:
             self.words_trie.insert(word)
 
-    def query(self, letter: str, verbose = False) -> bool:
+    def query(self, letter: str) -> bool:
         self.stream += letter
         if len(self.stream) > self.max_word_len:
             self.stream = self.stream[0 - self.max_word_len:]
 
-        if verbose:
-            print(f"query: {letter}")
-            print(f"stream: {self.stream}")
-
         for word_len in range(self.max_word_len):
             suffix = self.stream[-1 - word_len:]
-            if verbose:
-                print(f"suffix: {suffix}")
             
-            if TrieNode.search(suffix, self.words_trie, verbose):
+            if TrieNode.search(suffix, self.words_trie):
                 return True
 
         return False
@@ -48,33 +42,15 @@ class TrieNode:
             self.children[character_index].insert(word[1:])
 
     @classmethod
-    def search(cls, word: str, start_node, verbose) -> bool:
-        if verbose:
-            print(f"search for word {word}")
-
+    def search(cls, word: str, start_node) -> bool:
         current_node = start_node
 
         for character in word:
             character_index = cls.character_to_index[character]
 
             if current_node.children[character_index] == None:
-                if verbose:
-                    print(f"search found null child")
                 return False
             
-            # if current_node.children[character_index].isEndOfWord:
-            #     print("endofWord")
-            #     if len(word) == 1:
-            #         print("word is also len 1")
-            #         return True
-                
-            # if len(word) == 1:
-            #     print("search len 1")
-            #     return False
-            # #else:
-            #     #return self.children[character_index].search(word[1:])
-            
-            print(f"character {character}, next current node")
             current_node = current_node.children[character_index]
         
         return current_node.isEndOfWord
@@ -86,7 +62,7 @@ def test_example_1():
     assert streamChecker.query("a") == False
     assert streamChecker.query("b") == False
     assert streamChecker.query("c") == False
-    assert streamChecker.query("d", True) == True
+    assert streamChecker.query("d") == True
     assert streamChecker.query("e") == False
     assert streamChecker.query("f") == True
     assert streamChecker.query("g") == False
