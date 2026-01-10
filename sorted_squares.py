5945ae02c48059b8bc862534d0491137f87169f7
Chris Baker <baker.chris.3@gmail.com>
2026-01-10 10:26:13 -0800

    naive approach

--- sorted_squares.py
+++ sorted_squares.py
@@ -1,18 +1,19 @@
 from typing import List
 
 
-def sortedSquares(self, nums: List[int]) -> List[int]:
-    return nums
+def sortedSquares(nums: List[int]) -> List[int]:
+    return sorted([n * n for n in nums])
+    
 
 
 def test_example_1():
-    input = [-4,-1,0,3,10]
-    output = [0,1,9,16,100]
-    assert sortedSquares(input) == output
+    nums = [-4,-1,0,3,10]
+    expected = [0,1,9,16,100]
+    assert sortedSquares(nums) == expected
 
 
 def test_example_2():
-    input = [-7,-3,2,3,11]
-    output = [4,9,9,49,121]
-    assert sortedSquares(input) == output
+    nums = [-7,-3,2,3,11]
+    expected = [4,9,9,49,121]
+    assert sortedSquares(nums) == expected
 
