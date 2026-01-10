0f69d5754e1551ea95e9191ffddeab1cbff99949
Chris Baker <baker.chris.3@gmail.com>
2026-01-10 14:19:28 -0800

    solved

--- max_consecutive_ones_iii.py
+++ max_consecutive_ones_iii.py
@@ -3,7 +3,22 @@ from typing import List
 
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        return 0
+        answer = 0
+        left = 0
+        curr_zeros = 0
+
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                curr_zeros += 1
+            while(curr_zeros > k):
+                if nums[left] == 0:
+                    curr_zeros -= 1
+                left += 1
+            length = right - left + 1
+            if length > answer:
+                answer = length
+        
+        return answer
 
 
 def test_example_1():
