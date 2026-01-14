8342862c7c734943a1ac82c064c09bd40d226a52
Chris Baker <baker.chris.3@gmail.com>
2026-01-14 07:45:37 -0800

    provided solution

--- subarray_product_less_than_k.py
+++ subarray_product_less_than_k.py
@@ -4,7 +4,21 @@ from typing import List
 
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        return 0
+        if k <= 1:
+            return 0
+
+        ans = left = 0
+        curr = 1
+
+        for right in range(len(nums)):
+            curr *= nums[right]
+            while curr >= k:
+                curr //= nums[left]
+                left += 1 # A
+
+            ans += right - left + 1
+
+        return ans
 
 
 def test_example_1():
