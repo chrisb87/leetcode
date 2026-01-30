2e421d3201bf572abf511db413a66248fafbb3d9
Chris Baker <baker.chris.3@gmail.com>
2026-01-30 06:42:20 -0800

    solved ransomnote

--- ransomnote.py
+++ ransomnote.py
@@ -2,10 +2,25 @@
 
 # Each letter in magazine can only be used once in ransomNote.
 
+
+from collections import defaultdict
+
  
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        return False
+        magazineChars = defaultdict(int)
+        for c in magazine:
+            magazineChars[c] += 1
+
+        ransomNoteChars = defaultdict(int)
+        for c in ransomNote:
+            ransomNoteChars[c] += 1
+
+        for c in ransomNoteChars:
+            if magazineChars[c] < ransomNoteChars[c]:
+                return False
+
+        return True
 
 
 def test_example_1():
