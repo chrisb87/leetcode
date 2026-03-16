70bd5a2bdcebf1eb76399b6273bd76d49779ae24
Chris Baker <baker.chris.3@gmail.com>
2026-03-16 07:46:18 -0700

    solved divide chocolate

--- divide_chocolate.py
+++ divide_chocolate.py
@@ -3,7 +3,29 @@ from typing import List
 
 class Solution:
     def maximizeSweetness(self, sweetness: List[int], k: int) -> int:
-        return 0
+        def test(min_sweetness):
+            chunks = 0
+            curr = 0
+            for i in range(len(sweetness)):
+                curr += sweetness[i]
+                if curr >= min_sweetness:
+                    chunks += 1
+                    curr = 0
+                    if chunks == k + 1:
+                        return True
+            return False
+
+        left = min(sweetness)
+        right = sum(sweetness) // (k + 1)
+
+        while left < right:
+            mid = (left + right + 1) // 2
+            if test(mid):
+                left = mid
+            else:
+                right = mid - 1
+
+        return left
 
 
 def test_example_1():
@@ -20,7 +42,7 @@ def test_example_2():
     assert Solution().maximizeSweetness(sweetness, k) == answer
 
 
-def test_example_34():
+def test_example_3():
     sweetness = [1, 2, 2, 1, 2, 2, 1, 2, 2]
     k = 2
     answer = 5
